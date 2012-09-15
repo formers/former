aa95148ac8fff37f8e21fda69d30ce720e445dd9
Anahkiasen <ehtnam6@gmail.com>
2012-09-15 19:04:01 +0200

    Add ability to use ->select for ->value with Selects

--- libraries/Select.php
+++ libraries/Select.php
@@ -5,8 +5,18 @@ use \Form;
 
 class Select extends Field
 {
+  /**
+   * The select options
+   * @var array
+   */
   private $options = array();
 
+  /**
+   * Set the select options
+   *
+   * @param  array $options  The options as an array
+   * @param  mixed $selected Facultative selected entry
+   */
   public function options($options, $selected = null)
   {
     $this->options = $options;
@@ -14,6 +24,21 @@ class Select extends Field
     if($selected) $this->value = $selected;
   }
 
+  /**
+   * Select a particular list item
+   *
+   * @param  mixed $selected Selected item
+   */
+  public function select($selected)
+  {
+    $this->value = $selected;
+  }
+
+  /**
+   * Renders the select
+   *
+   * @return string A <select> tag
+   */
   public function __toString()
   {
     if($this->type == 'multiselect') $this->multiple();
