6461611123c7b315a3521e818743b0028afaaaca
Anahkiasen <ehtnam6@gmail.com>
2013-01-18 20:52:11 +0100

    Fix Laravel 3 build

--- start.php
+++ start.php
@@ -7,6 +7,3 @@ Autoloader::namespaces(array(
   'Illuminate' => Bundle::path('former') . 'vendor' .DS. 'illuminate/container/Illuminate',
   'Underscore' => Bundle::path('former') . 'vendor' .DS. 'anahkiasen/underscore-php/src/Underscore',
 ));
-
-include 'vendor/meido/form/src/Meido/Form/Form.php';
-include 'vendor/meido/html/src/Meido/HTML/HTML.php';
