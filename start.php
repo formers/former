4937f8826af66ab98894f8ed64cde4981df3fb13
Maxime Fabre <ehtnam6@gmail.com>
2013-03-04 17:31:25 +0100

    Fix autoloading of HtmlObject in Laravel 3

--- start.php
+++ start.php
@@ -4,6 +4,7 @@
 
 Autoloader::namespaces(array(
   'Former'     => Bundle::path('former') . 'src'    .DS. 'Former',
+  'HtmlObject' => Bundle::path('former') . 'vendor' .DS. 'anahkiasen/html-object/src/HtmlObject',
   'Illuminate' => Bundle::path('former') . 'vendor' .DS. 'illuminate/container/Illuminate',
   'Underscore' => Bundle::path('former') . 'vendor' .DS. 'anahkiasen/underscore-php/src/Underscore',
 ));
