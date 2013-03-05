e065eaaee57a87bf63a64a8d61926354ec841b05
Maxime Fabre <ehtnam6@gmail.com>
2013-03-05 14:59:03 +0100

    Fix #76 - Install on L3 fails

--- start.php
+++ start.php
@@ -1,10 +1,17 @@
 <?php
 
+$vendor = Bundle::path('former') . 'vendor' .DS;
+
 // Loading Former -------------------------------------------------- /
 
 Autoloader::namespaces(array(
-  'Former'     => Bundle::path('former') . 'src'    .DS. 'Former',
-  'HtmlObject' => Bundle::path('former') . 'vendor' .DS. 'anahkiasen/html-object/src/HtmlObject',
-  'Illuminate' => Bundle::path('former') . 'vendor' .DS. 'illuminate/container/Illuminate',
-  'Underscore' => Bundle::path('former') . 'vendor' .DS. 'anahkiasen/underscore-php/src/Underscore',
+  'Former'     => Bundle::path('former') . 'src' .DS. 'Former',
+  'HtmlObject' => $vendor . 'anahkiasen/html-object/src/HtmlObject',
+  'Illuminate' => $vendor . 'illuminate/container/Illuminate',
+  'Underscore' => $vendor . 'anahkiasen/underscore-php/src/Underscore',
 ));
+
+// Load Str class -------------------------------------------------- /
+
+include $vendor . 'illuminate/support/Illuminate/Support/Str.php';
+include $vendor . 'illuminate/support/Illuminate/Support/Pluralizer.php';
