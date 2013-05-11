534d8b1ea184412bf584371854657835c27ec7c4
Joshua Stine <joshua.stine@gmail.com>
2013-05-11 15:32:59 -0400

    Added support for retrieving model methods in the array helper.

--- src/Former/Helpers.php
+++ src/Former/Helpers.php
@@ -115,7 +115,10 @@ class Helpers
       if(is_array($model)) $model = (object) $model;
 
       // Calculate the value
-      if($value) $modelValue = $model->$value;
+      // Calculate the value
+      if(is_string($model) and !isset($model->$value) and isset($value)) $modelValue = $value; // string or number
+      elseif(!is_string($model) and !isset($model->$value)) $modelValue = $model->$value; // accesing a model method
+      elseif($value and isset($model->$value)) $modelValue = $model->$value; // accessing a model atrtribute
       elseif(method_exists($model, '__toString')) $modelValue = $model->__toString();
       else $modelValue = null;
 
