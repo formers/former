57c7dfd7b75f4216856e43345e7cca0b7f4783b0
Raphael Mobis Tacla <r.mobis@gmail.com>
2013-05-13 21:46:46 -0300

    Allow live validation rules to be passed as array

--- tests/LiveValidationTest.php
+++ tests/LiveValidationTest.php
@@ -51,9 +51,38 @@ class LiveValidationTest extends FormerTests
     $this->assertHTML($this->matchField(array(), 'email', 'bar'), $email);
   }
 
+  public function testCanUseMultipleRulesArrayAsArray()
+  {
+    $this->former->withRules(array('foo' => array('required')), array('bar' => array('email')));
+
+    // First field
+    $input = $this->former->text('foo')->__toString();
+
+    $this->assertLabel($input, 'foo', true);
+    $this->assertHTML($this->matchControlGroup(), $input);
+    $this->assertHTML($this->matchField(array('required' => 'true')), $input);
+
+    // Second field
+    $email = $this->former->text('bar')->__toString();
+
+    $this->assertLabel($email, 'bar');
+    $this->assertHTML($this->matchControlGroup(), $email);
+    $this->assertHTML($this->matchField(array(), 'email', 'bar'), $email);
+  }
+
   public function testCanSetFieldAsRequired()
   {
     $this->former->withRules(array('foo' => 'required'));
+    $input = $this->former->text('foo')->__toString();
+
+    $this->assertHTML($this->matchField(array('required' => 'true')), $input);
+    $this->assertLabel($input, 'foo', true);
+    $this->assertHTML($this->matchControlGroup(), $input);
+  }
+
+  public function testCanSetFieldAsRequiredAsArray()
+  {
+    $this->former->withRules(array('foo' => array('required')));
     $input = $this->former->text('foo')->__toString();
 
     $this->assertHTML($this->matchField(array('required' => 'true')), $input);
@@ -74,9 +103,33 @@ class LiveValidationTest extends FormerTests
     $this->assertHTML($label, $input);
   }
 
+  public function testCanAddRequiredTextToPlainFieldsAsArray()
+  {
+    $this->former->close();
+    $this->former->withRules(array('foo' => array('required')));
+
+    $input = $this->former->text('foo')->__toString();
+    $label = $this->matchLabel('foo', 'foo', true);
+    unset($label['attributes']['class']);
+
+    $this->assertHTML($this->matchField(array('required' => 'true')), $input);
+    $this->assertHTML($label, $input);
+  }
+
   public function testCanSetMaxToText()
   {
     $this->former->withRules(array('foo' => 'max:42'));
+
+    $input = $this->former->text('foo')->__toString();
+    $matcher = $this->matchField(array('maxlength' => '42'));
+
+    $this->assertHTML($matcher, $input);
+    $this->assertControlGroup($input);
+  }
+
+  public function testCanSetMaxToTextAsArray()
+  {
+    $this->former->withRules(array('foo' => array('max:42')));
 
     $input = $this->former->text('foo')->__toString();
     $matcher = $this->matchField(array('maxlength' => '42'));
@@ -96,6 +149,17 @@ class LiveValidationTest extends FormerTests
     $this->assertControlGroup($input);
   }
 
+  public function testCanSetMaxToNumberAsArray()
+  {
+    $this->former->withRules(array('foo' => array('max:42')));
+
+    $input = $this->former->number('foo')->__toString();
+    $matcher = $this->matchField(array('max' => '42'), 'number');
+
+    $this->assertHTML($matcher, $input);
+    $this->assertControlGroup($input);
+  }
+
   public function testCanSetMinToText()
   {
     $this->former->withRules(array('foo' => 'min:42'));
@@ -107,9 +171,31 @@ class LiveValidationTest extends FormerTests
     $this->assertControlGroup($input);
   }
 
+  public function testCanSetMinToTextAsArray()
+  {
+    $this->former->withRules(array('foo' => array('min:42')));
+
+    $input = $this->former->text('foo')->__toString();
+    $matcher = $this->matchField(array('minlength' => '42'));
+
+    $this->assertHTML($matcher, $input);
+    $this->assertControlGroup($input);
+  }
+
   public function testCanSetMinToNumber()
   {
     $this->former->withRules(array('foo' => 'min:42'));
+
+    $input = $this->former->number('foo')->__toString();
+    $matcher = $this->matchField(array('min' => '42'), 'number');
+
+    $this->assertHTML($matcher, $input);
+    $this->assertControlGroup($input);
+  }
+
+  public function testCanSetMinToNumberAsArray()
+  {
+    $this->former->withRules(array('foo' => array('min:42')));
 
     $input = $this->former->number('foo')->__toString();
     $matcher = $this->matchField(array('min' => '42'), 'number');
@@ -132,9 +218,36 @@ class LiveValidationTest extends FormerTests
     $this->assertHTML($matcher, $input);
   }
 
+  /**
+   * @dataProvider providePatterns
+   */
+  public function testCanApplyPatternsToFieldsAsArray($type, $pattern)
+  {
+    $type = explode('|', $type);
+
+    $this->former->withRules(array('foo' => $type));
+
+    $input = $this->former->text('foo')->__toString();
+    $matcher = $this->matchField(array('pattern' => $pattern));
+
+    $this->assertControlGroup($input);
+    $this->assertHTML($matcher, $input);
+  }
+
   public function testCanSetNumberFieldAsNumeric()
   {
     $this->former->withRules(array('foo' => 'numeric'));
+
+    $input = $this->former->number('foo')->__toString();
+    $matcher = $this->matchField(array('step' => 'any'), 'number');
+
+    $this->assertControlGroup($input);
+    $this->assertHTML($matcher, $input);
+  }
+
+  public function testCanSetNumberFieldAsNumericAsArray()
+  {
+    $this->former->withRules(array('foo' => array('numeric')));
 
     $input = $this->former->number('foo')->__toString();
     $matcher = $this->matchField(array('step' => 'any'), 'number');
@@ -154,9 +267,31 @@ class LiveValidationTest extends FormerTests
     $this->assertHTML($matcher, $input);
   }
 
+  public function testCanSetDateAsBeforeSomethingAsArray()
+  {
+    $this->former->withRules(array('foo' => array('before:2012-03-03')));
+
+    $input = $this->former->date('foo')->__toString();
+    $matcher = $this->matchField(array('max' => '2012-03-03'), 'date');
+
+    $this->assertControlGroup($input);
+    $this->assertHTML($matcher, $input);
+  }
+
   public function testCanSetDateAsAfterSomething()
   {
     $this->former->withRules(array('foo' => 'after:2012-03-03'));
+
+    $input = $this->former->date('foo')->__toString();
+    $matcher = $this->matchField(array('min' => '2012-03-03'), 'date');
+
+    $this->assertControlGroup($input);
+    $this->assertHTML($matcher, $input);
+  }
+
+  public function testCanSetDateAsAfterSomethingAsArray()
+  {
+    $this->former->withRules(array('foo' => array('after:2012-03-03')));
 
     $input = $this->former->date('foo')->__toString();
     $matcher = $this->matchField(array('min' => '2012-03-03'), 'date');
@@ -176,9 +311,31 @@ class LiveValidationTest extends FormerTests
     $this->assertHTML($matcher, $input);
   }
 
+  public function testCanRestrictMimeTypesAsArray()
+  {
+    $this->former->withRules(array('foo' => array('mimes:jpg,gif')));
+
+    $input = $this->former->file('foo')->__toString();
+    $matcher = $this->matchField(array('accept' => 'image/jpeg,image/gif'), 'file');
+
+    $this->assertControlGroup($input);
+    $this->assertHTML($matcher, $input);
+  }
+
   public function testCanForceFieldToImage()
   {
     $this->former->withRules(array('foo' => 'image'));
+
+    $input = $this->former->file('foo')->__toString();
+    $matcher = $this->matchField(array('accept' => 'image/jpeg,image/png,image/gif,image/bmp'), 'file');
+
+    $this->assertControlGroup($input);
+    $this->assertHTML($matcher, $input);
+  }
+
+  public function testCanForceFieldToImageAsArray()
+  {
+    $this->former->withRules(array('foo' => array('image')));
 
     $input = $this->former->file('foo')->__toString();
     $matcher = $this->matchField(array('accept' => 'image/jpeg,image/png,image/gif,image/bmp'), 'file');
@@ -201,9 +358,36 @@ class LiveValidationTest extends FormerTests
     $this->assertHTML($matcher, $input);
   }
 
+  /**
+   * @dataProvider provideTypes
+   */
+  public function testCanSwitchTypesAccordingToRulesAsArray($type)
+  {
+    $type = explode('|', $type);
+
+    $this->former->withRules(array('foo' => $type));
+
+    $input = $this->former->text('foo')->__toString();
+    $matcher = $this->matchField(array(), $type);
+
+    $this->assertControlGroup($input);
+    $this->assertHTML($matcher, $input);
+  }
+
   public function testCanSetBoundariesToText()
   {
     $this->former->withRules(array('foo' => 'between:1,10'));
+
+    $input = $this->former->text('foo')->__toString();
+    $matcher = $this->matchField(array('minlength' => '1', 'maxlength' => '10'));
+
+    $this->assertControlGroup($input);
+    $this->assertHTML($matcher, $input);
+  }
+
+  public function testCanSetBoundariesToTextAsArray()
+  {
+    $this->former->withRules(array('foo' => array('between:1,10')));
 
     $input = $this->former->text('foo')->__toString();
     $matcher = $this->matchField(array('minlength' => '1', 'maxlength' => '10'));
@@ -223,11 +407,35 @@ class LiveValidationTest extends FormerTests
     $this->assertHTML($matcher, $input);
   }
 
+  public function testCanSetBoundariestoNumberAsArray()
+  {
+    $this->former->withRules(array('foo' => array('between:1,10')));
+
+    $input = $this->former->number('foo')->__toString();
+    $matcher = $this->matchField(array('min' => '1', 'max' => '10'), 'number');
+
+    $this->assertControlGroup($input);
+    $this->assertHTML($matcher, $input);
+  }
+
   public function testCanDisableLiveValidation()
   {
     // Change config
     $this->app->app['config'] = $this->app->getConfig(false);
     $this->former->withRules(array('foo' => 'required'));
+
+    $input = $this->former->text('foo')->__toString();
+    $matcher = $this->matchField();
+
+    $this->assertHTML($matcher, $input);
+    $this->assertControlGroup($input);
+  }
+
+  public function testCanDisableLiveValidationAsArray()
+  {
+    // Change config
+    $this->app->app['config'] = $this->app->getConfig(false);
+    $this->former->withRules(array('foo' => array('required')));
 
     $input = $this->former->text('foo')->__toString();
     $matcher = $this->matchField();
