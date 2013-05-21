cb6be3c8e68cd75c396969ab8cf2c25dea6575ad
Joshua Stine <joshua.stine@gmail.com>
2013-05-21 17:30:12 -0400

    Added support for accessing model methods.
    Signed-off-by: Joshua Stine <joshua.stine@gmail.com>

--- src/Former/Helpers.php
+++ src/Former/Helpers.php
@@ -115,10 +115,9 @@ class Helpers
       if(is_array($model)) $model = (object) $model;
 
       // Calculate the value
-      // Calculate the value
-      if(is_string($model) and !isset($model->$value) and isset($value)) $modelValue = $value; // string or number
-      elseif(!is_string($model) and !isset($model->$value)) $modelValue = $model->$value; // accesing a model method
-      elseif($value and isset($model->$value)) $modelValue = $model->$value; // accessing a model atrtribute
+      if(is_string($model) and !isset($model->$value)) $modelValue = $value; // string or number
+      elseif(!is_string($model) and !isset($model->$value)) $modelValue = $model->$value; // access a model method
+      elseif($value and isset($model->$value)) $modelValue = $model->$value; // access a model atrtribute
       elseif(method_exists($model, '__toString')) $modelValue = $model->__toString();
       else $modelValue = null;
 
