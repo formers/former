a588c25be3077a7a736f88d4a09d8ea94ade01a3
Maxime Fabre <ehtnam6@gmail.com>
2013-11-05 13:27:28 +0100

    Fix form-control class being added to radios

--- src/config/TwitterBootstrap3.php
+++ src/config/TwitterBootstrap3.php
@@ -4,29 +4,23 @@ return array(
 
 	// Map Former-supported viewports to Bootstrap 3 equivalents
 	'viewports' => array(
-
-		'large' => 'lg',
+		'large'  => 'lg',
 		'medium' => 'md',
-		'small' => 'sm',
-		'mini' => 'xs',
-
+		'small'  => 'sm',
+		'mini'   => 'xs',
 	),
 
 	// Width of labels for horizontal forms expressed as viewport => grid columns
 	'labelWidths' => array(
-
 		'large' => 2,
 		'small' => 4,
-
 	),
 
 	// HTML markup and classes used by Bootstrap 3 for icons
 	'icon' => array(
-
-		'tag' => 'span',
-		'set' => 'glyphicon',
+		'tag'    => 'span',
+		'set'    => 'glyphicon',
 		'prefix' => 'glyphicon',
-
 	),
 
 );
