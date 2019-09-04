79831dce4ed91f3c00139e88b37fb47babff510f
Alex Bouma <me@alexbouma.me>
2019-09-04 19:55:48 +0200

    Allow tests to run in a Laravel 6 environment

---
+++ tests/TestCases/MacroableContainer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Former\TestCases;
+
+use Illuminate\Container\Container;
+use Illuminate\Support\Traits\Macroable;
+
+class MacroableContainer extends Container
+{
+	use Macroable;
+}
