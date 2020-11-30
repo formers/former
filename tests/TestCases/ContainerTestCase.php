1337fcbbbba3a27a566c7ca68ccd8d10a217f615
Alex Bouma <me@alexbouma.me>
2020-11-30 21:30:46 +0100

    Fix missing mocked config attribute

--- tests/TestCases/ContainerTestCase.php
+++ tests/TestCases/ContainerTestCase.php
@@ -114,6 +114,7 @@ abstract class ContainerTestCase extends TestCase
 		$defaults = include realpath(__DIR__.'/../../src/config/former.php');
 
 		$options = array_merge($defaults, array(
+			'absolute'                            => true,
 			'automatic_label'                     => true,
 			'capitalize_translations'             => true,
 			'default_form_type'                   => 'horizontal',
